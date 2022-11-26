c8e3169fe627fe18dd2e8dbe98d59d0eaed63295
goodenough406 <107664784+goodenough406@users.noreply.github.com>
2022-11-26 11:51:56 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <router-view />
     <h1>沈理树洞</h1>
+    <div>
+      <i class="toutiao icon-dianzan"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import './styles/index.less'
-
+// 注册使用vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
